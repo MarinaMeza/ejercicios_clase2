7b7bca01c660714f1534eec66ac753aa3047f015
alumno <alumno>
2017-03-27 19:53:15 -0300

    Ejericio 15 Segundo commit

---
+++ AplicacionN15/figGeometrica.php
@@ -0,0 +1,38 @@
+<?php
+    class FiguraGeometrica
+    {
+        protected $_color;
+        protected $_superficie;
+        protected $_perimetro;
+
+        public function __construct()
+        {
+            
+        }
+
+        public function GetColor()
+        {
+            return $this->_color;
+        }
+
+        public function SetColor($color)
+        {
+            $this->color=$color;
+        }
+
+         protected function ToString()
+         {
+
+         }
+
+         public abstract function Dibujar()
+         {
+
+         }
+
+         protected abstract function CalcularDatos()
+         {
+             
+         }
+    }
+?>
